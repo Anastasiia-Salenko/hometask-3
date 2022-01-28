7ef4c07b58792285ab208e85a544d62115ddcc8a
Anastasiia Salenko <batsvinova@gmail.com>
2022-01-28 11:28:58 +0200

    Added cors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
 import { router as noteListRouter } from "./layers/routes/note-list.routes.js";
 import { router as noteItemRouter } from "./layers/routes/note-item.routes.js";
 import { router as noteStatisticsRouter } from "./layers/routes/note-statistics.routes.js";
@@ -8,6 +9,7 @@ import { setupSwagger } from "./docs/setup-swagger.js";
 
 export const app = express();
 
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 
